e30c1437754b71ee5ca589ee69bfc25e8900ce03
silatcha <75568198+silatcha@users.noreply.github.com>
2022-03-12 12:01:39 +0100

    hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Description: Prints "Programing is like building a multilingual puzle"
+ *
+ *Return: Always 0 for success
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Prints "with proper grammar, but the outcome is a piece of
+ * art,"
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
