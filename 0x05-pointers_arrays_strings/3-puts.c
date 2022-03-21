5b14e3d06f5acca0f382f57e31828257f32c3d43
silatcha <silatchayvan@gmail.com>
2022-03-21 15:20:44 -0700

    pointer

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,17 +2,17 @@
 
 /**
  * _puts - stdout
- * @s: pointer s
+ * @str: pointer str
  * Return: void
  */
 
-void _puts(char *s)
+void _puts(char *str)
 {
 int i=0;
 
-while (s[i] != '\0')
+while (str[i] != '\0')
 {
-_putchar(s[i]);
+_putchar(stri]);
 i++;
 }
 
